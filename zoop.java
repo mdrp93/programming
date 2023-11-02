da5cdf64cebcdfa4dc6d7041f29bb9312f3aa046
mdrp93 <mariadiazrp@outlook.com>
2023-11-02 22:57:11 +0100

    create zoop.java

--- zoop.java
+++ zoop.java
@@ -22,3 +22,8 @@ public class zoop {
     }
     
 }
+
+//Draw a stack diagram that shows the state of the program the second time zoop is invoked. What is the complete output?
+// Zoop = fred [breakfast] bob [fork]
+// fork in this moment is equal 2 * buzz , buzz is equal 2, so fork is equal 4.
+//Solution zoop diagram {fred [breakfast] bob [4]}
