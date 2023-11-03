eb6c7fd10eef2aa50efd55c445fc7d738b556857
mdrp93 <mariadiazrp@outlook.com>
2023-11-03 02:39:37 +0100

    create multadd.java

--- Multadd.java
+++ Multadd.java
@@ -18,23 +18,23 @@ public static void expSum(double x) {
 
 
 public static void main(String[] args) {
-    System.out.print("Result to (1*2+3)= ");
-        multadd (1.0, 2.0, 3.0);
-
+        System.out.print("Result to (1*2+3)= ");
+        multadd (1.0, 2.0, 3.0) ;
+        System.out.println(" ");
       
         double a1 = Math.cos(Math.PI/4.0);      //result to a1 (cos (pi/4)) is a in method multadd
 		double b1 =  1.0/2.0;       // division to take half of a and multiply it in multadd (a*b)
 		double c1 = Math.sin(Math.PI/4.0);       //result to c1 (sin(pi/4)) is c in method multadd
 		System.out.print("sin(pi/4) + cos(pi/4)/2 = "); 
         multadd (a1, b1, c1); 
-        
+        System.out.println(" ");
 
         double a2= 1.0;
         double b2 = Math.log(10);
         double c2 = Math.log ( 20);
         System.out.print("log(10)+log(20)= ");
         multadd (a2, b2, c2);  
-        System.out.println();
+        System.out.println(" ");
         
         System.out.print("Please insert number to calculate x in this equation:[xe^(-x) + sqrt(1 - e^(-x))] \n x = ");
         Scanner in = new Scanner(System.in); 
