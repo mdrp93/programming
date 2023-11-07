3232d6271063af2bbc921bf79cb0e0f5aa38801c
Maria Díaz RP <146107673+mdrp93@users.noreply.github.com>
2023-11-07 19:59:11 +0100

    Add files via upload

---
+++ Ex06.java
@@ -0,0 +1,26 @@
+public class Ex06 {
+    final static double MIN= 0.001; 
+
+    public static double squareRoot(double a){
+        double x1 = 0;
+        double x0 = a/2;
+        
+
+        while (Math.abs(x0-x1)>MIN){
+            x1 = x0;
+            x0= (x0 + a/x0);       
+        }
+
+        
+    return x0;
+        }
+    
+
+    
+    public static void main (String[] args){
+        double x [] = {9, 16, 25, 49, 64 };
+        for (int i=0; i<5; i++){
+        System.out.printf("sqrt (%.2f) is %.2f\n", x [i], squareRoot(x[i]));
+        }
+    }
+}
