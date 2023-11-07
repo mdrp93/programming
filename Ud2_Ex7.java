9e280f6045699ba5edafd561c3966962b6dd4d59
Maria Díaz RP <146107673+mdrp93@users.noreply.github.com>
2023-11-07 20:58:50 +0100

    Add files via upload

---
+++ Ud2_Ex7.java
@@ -0,0 +1,29 @@
+
+
+    public class Ud2_Ex7  {
+    
+
+        public static double gauss(double x, int n) {
+            double suma = 0;
+            double termino = 1;
+    
+            for (int i = 0; i < n; i++) {
+                suma += termino;
+                termino *= (-1) * x * x / (i + 1);
+            }
+    
+            return suma;
+        }
+    
+        public static void main(String[] args) {
+            double x = 2.0;
+            int n = 5;
+    
+            double resultado = gauss(x, n);
+            
+            System.out.printf("Para x = 1 la solucion es %.2f\n", Math.exp(x));
+            System.out.println("El resultado de la serie es: " + resultado);
+        
+    }}
+        
+    
