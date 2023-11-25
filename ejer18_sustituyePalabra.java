89fd8bc0991daf43db2d150167d9f015491a042d
mdrp93 <mariadiazrp@outlook.com>
2023-11-25 22:54:55 +0100

    upload ejer18cadenas

---
+++ ejer18_sustituyePalabra.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class ejer18_sustituyePalabra{
+        
+    public static String sustituyePalabra(String cadena, String palabra1, String palabra2) {
+        cadena=cadena.toUpperCase(); //paso todo a mayúsculas para evitar problemas de búsqueda
+        palabra1=palabra1.toUpperCase();
+        palabra2=palabra2.toUpperCase();
+
+        String resultado = cadena.replace(palabra1, palabra2); //uso la función replace que cambia una palabra por otra.
+       
+          
+        return resultado;
+    } 
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in); 
+        
+        System.out.println("Escribe una frase: ");
+        String frase = in.nextLine();   
+           
+        System.out.println("Escribe la palabra que se va a sustituir: ");
+        String palabra1 = in.nextLine();
+           
+        System.out.println("Escribe la palabra nueva: ");
+        String palabra2 = in.nextLine();
+      
+        String solution = sustituyePalabra(frase, palabra1, palabra2);
+        System.out.println(solution);
+        }
+    }
+    
+
