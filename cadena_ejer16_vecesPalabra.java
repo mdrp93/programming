c28f22fcca689d509946ba5198ce1f2907aa04a8
mdrp93 <mariadiazrp@outlook.com>
2023-11-26 01:09:35 +0100

    modified ejer 16

--- cadena_ejer16_vecesPalabra.java
+++ cadena_ejer16_vecesPalabra.java
@@ -12,6 +12,7 @@ public class cadena_ejer16_vecesPalabra{
         
         // Iterar mientras se encuentre la palabra en la cadena con indexOf. 
         while ((indice = cadena.indexOf(palabra, indice)) != -1) {  // El método indexOf devuelve la posición de la palabra en la cadena, o -1 si no se encuentra--> el -1 es como si fuera un false. 
+            //Dentro del bucle, se utiliza el método indexOf para buscar la próxima aparición de la palabra en la cadena. Si encuentra la palabra, el método devuelve la posición donde comienza la palabra en la cadena y se asigna ese valor a la variable indice.
             contador++;
             indice += palabra.length(); //sigue desde la siguiente posición para no volver a contar lo anterior.
         }
